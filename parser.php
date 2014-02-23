38db2bf947adaf22b7ea246e60a8c25ef27cc0c6
Timothy Onggowasito <timong09@gmail.com>
2014-02-23 17:29:55 -0500

    Allow users to define their own delimiter.

--- parser.php
+++ parser.php
@@ -32,9 +32,14 @@ class DSTAT {
 		$this->values = $this->getValues();
 	}
 
+	public function getDelimiter() {
+		return $this->delimiter;
+	}
+
 	public function getKeys() {
 		$output_key = null;
-		preg_match_all("/^[\^][^\^]\b\w*/sm", $this->text, $output_key);
+		// preg_match_all("/^[\^][^\^]\b\w*/sm", $this->text, $output_key);
+		preg_match_all($this->generateKeyRegex(), $this->text, $output_key);
 		$result = array_map(array(__CLASS__, "removeKeyDelimiter"), $output_key);
 		return $result[0];
 	}
@@ -42,6 +47,7 @@ class DSTAT {
 	public function getValues() {
 		$output_value = null;
 		preg_match_all("/^[\^]{3}(.*?)[\^]{3}/sm", $this->text, $output_value);
+		preg_match_all($this->generateValueRegex(), $this->text, $output_value);
 		return array_map("trim", $output_value[1]);
 	}
 
@@ -53,6 +59,21 @@ class DSTAT {
 		return $result_array;
 	}
 
+	private function generateKeyRegex() {
+		$delim = $this->delimiter["key"];
+		$escaped_delim = preg_quote($delim, $delim);
+		$regular_expression = "/^[".$escaped_delim."]{".strlen($delim)."}[^".$escaped_delim."]\b\w*/sm";
+		return $regular_expression;
+	}
+
+	private function generateValueRegex() {
+		$delim = $this->delimiter["key"];
+		$delim_count = strlen($this->delimiter["value"]);
+		$escaped_delim = preg_quote($delim, $delim);
+		$regular_expression = "/^[".$escaped_delim."]{".$delim_count."}(.*?)[".$escaped_delim."]{".$delim_count."}/sm";
+		return $regular_expression;
+	}
+
 	public function isKeyAndValueMatching() {
 		$result = true;
 		if(count($this->keys) != count($this->values)) {
@@ -61,13 +82,16 @@ class DSTAT {
 		return $result;
 	}
 
-	private static function removeKeyDelimiter($text) {
-		return preg_replace("/^[\/^][^\/^]/sm", "", $text);
+	private function removeKeyDelimiter($text) {
+		$delim = $this->delimiter["key"];
+		$delim_count = strlen($delim);
+		$escaped_delim = preg_quote($delim, $delim);
+		return preg_replace("/^[".$escaped_delim."]{".$delim_count."}[^".$escaped_delim."]/sm", "", $text);
 	}
 
 }
 
-$text = "
+$example_text_1 = "
 ^ Test
 ^^^ Test test test
 test test test
@@ -78,8 +102,25 @@ test test test
 ^^^
 ";
 
-$test = new DSTAT($text, '^', '^^^');
+$example_text_2 = "
+~~ Test1
+~~~~ Test test test
+test test test
+~~~~
+~~ Test2342
+~~~~ Test test test
+test test test
+~~~~
+
+
+-- Asdasdk;lcfirst
+Asdasdk
+##Hello World##
+";
+
+$test = new DSTAT($example_text_1, '^', '^^^');
 echo $test->text;
+var_dump($test->getDelimiter());
 var_dump($test->getKeys());
 var_dump($test->getValues());
 var_dump($test->getBoth());
